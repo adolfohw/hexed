344c123af2c3e6dc903c9ddb79b96c00367fe2ce
Adolfo H. Wernke <adolfohw@gmail.com>
2020-02-15 18:40:40 -0300

    Add coloring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use std::{
     env, fs,
     io::{self, Write},
@@ -8,16 +9,28 @@ const ROW_SIZE: usize = 0x10;
 
 // Print without panicking
 macro_rules! safe_print {
-    ($($arg:tt)*) => {
+    (in $($color:ident)+: $($arg:tt)*) => {{
+        let msg = format!("{}", format_args!($($arg)*));
+        if write!(io::stdout(), "{}", msg$(.$color())+).is_err() {
+            return;
+        }
+    }};
+
+    ($($arg:tt)*) => {{
         if write!(io::stdout(), "{}", format_args!($($arg)*)).is_err() {
             return;
         }
-    };
+    }};
 }
 
 macro_rules! safe_println {
     () => {
         safe_print!("\n");
+    };
+
+    (in $($color:ident)+: $($arg:tt)*) => {
+        safe_print!(in $($color)+: $($arg)*);
+        safe_println!();
     };
 
     ($($arg:tt)*) => {
@@ -26,27 +39,47 @@ macro_rules! safe_println {
     }
 }
 
+macro_rules! safe_eprint {
+    ($($arg:tt)*) => {{
+        let msg = format!("{}", format_args!($($arg)*));
+        if write!(io::stderr(), "{}", msg.red()).is_err() {
+            return;
+        }
+    }};
+}
+
+macro_rules! safe_eprintln {
+    () => {
+        safe_eprint!("\n");
+    };
+
+    ($($arg:tt)*) => {
+        safe_eprint!($($arg)*);
+        safe_eprintln!();
+    };
+}
+
 fn main() {
     let filler: String = iter::repeat(' ').take(ROW_SIZE * 3).collect();
     let path = match env::args().nth(1) {
         Some(path) => path,
         None => {
-            safe_println!("You must provide a path");
+            safe_eprintln!("You must provide a path");
             return;
         }
     };
     let file = match fs::read(path) {
         Ok(bytes) => bytes,
         Err(_) => {
-            safe_println!("Invalid path");
+            safe_eprintln!("Invalid path");
             return;
         }
     };
 
     // Header
-    safe_print!("  Offset ");
+    safe_print!(in bold green: "  Offset ");
     for offset in 0..ROW_SIZE {
-        safe_print!("{:02X} ", offset);
+        safe_print!(in green: "{:02X} ", offset);
     }
     safe_println!();
 
@@ -54,26 +87,36 @@ fn main() {
     let rows = file.len() / ROW_SIZE + (file.len() % ROW_SIZE).min(1);
     for row in 0..rows {
         // Offset magnitude
-        safe_print!("{:08X} ", row * ROW_SIZE);
+        safe_print!(in green: "{:08X} ", row * ROW_SIZE);
         // Row dump
         let start = row * ROW_SIZE;
         let end = (start + ROW_SIZE).min(file.len());
         // Bytes
         let mut safe_printed_bytes = 0;
         for byte in &file[start..end] {
-            safe_print!("{:02X} ", byte);
+            match byte {
+                // Null
+                0 => safe_print!(in bright_black: "{:02X} ", byte),
+                // Non-printable ASCII
+                0x01..=0x1F | 0x7F => safe_print!(in bright_yellow: "{:02X} ", byte),
+                // Printable ASCII
+                b' '..=b'~' => safe_print!(in bright_cyan: "{:02X} ", byte),
+                // Other
+                _ => safe_print!("{:02X} ", byte),
+            }
             safe_printed_bytes += 1;
         }
         if safe_printed_bytes < ROW_SIZE {
             safe_print!("{}", &filler[0..(ROW_SIZE - safe_printed_bytes) * 3]);
         }
+        safe_print!(" ");
         // Text repr
         for byte in &file[start..end] {
             let mut ch = char::from(*byte);
             if !ch.is_ascii_graphic() {
-                ch = '⋅';
+                ch = ' ';
             }
-            safe_print!("{}", ch);
+            safe_print!(in bright_black: "{}", ch);
         }
         safe_println!();
     }
